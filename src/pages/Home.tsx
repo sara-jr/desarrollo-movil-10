3b25a57440348e4200f95dded23cd5eebbaa830b
sara-jr <salazar.ramirez.j@hotmail.com>
2024-10-24 16:37:06 -0600

    Creada la pantalla de inicio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBadge, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
+import { notificationsOutline, searchOutline, locationOutline, addOutline } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonButtons slot='start'>
+            <IonButton id='search-btn' size='large'>
+              <IonIcon slot='icon-only' icon={searchOutline}></IonIcon>
+            </IonButton>
+          </IonButtons>
+          <IonTitle style={{'text-align': 'center'}}>Inicio</IonTitle>
+          <IonButtons slot='end'>
+            <IonButton id='notifications-btn' fill='solid'>
+              <IonBadge id='notification-count'>1</IonBadge>
+              <IonIcon slot='end' icon={notificationsOutline}></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow>
+            <IonCol size='12' class='ion-align-items-center ion-justify-content-center'>
+              <IonButton expand='block'>
+                <IonIcon icon={locationOutline} slot='start'>
+                </IonIcon>
+                Buscar rutas cercanas a mí
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol size='12' class='ion-align-items-center ion-justify-content-center'>
+              <IonButton color='warning' expand='block'>
+                <IonIcon icon={addOutline} slot='start'>
+                </IonIcon>
+                Crear ruta
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
