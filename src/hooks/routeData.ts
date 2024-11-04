16fa4216d84addbf303e0fd6860d8a98a03b4c58
sara-jr <salazar.ramirez.j@hotmail.com>
2024-11-03 19:16:56 -0600

    Implementado el registro de rutas en la base de datos
    Creado hook useRouteData para manipular y crear rutas con los siguientes metodos
      getRouteById
      getRoutes
      createRoute
      updateRouteById
      deleteRouteById:
    Implementada la funcionalidad para crear una ruta mediante el hook en AddRoute

---
+++ src/hooks/routeData.ts
@@ -0,0 +1,84 @@
+import { useIonAlert } from '@ionic/react';
+import { Permission, Role } from 'appwrite';
+import { useContext } from 'react';
+import { database } from '../config/appwrite';
+import { AuthContext } from '../providers/AuthProvider';
+
+const DB_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const ROUTES_COLLECTION_ID = import.meta.env.VITE_APPWRITE_ROUTES_COLLECTION_ID;
+
+
+export interface Route{
+  name: string,
+  price: number,
+  points: RoutePoint[],
+  starts_at: string,
+  ends_at: string,
+}
+
+
+export interface RoutePoint{
+  longitude: number,
+  latitude: number
+}
+
+
+export const useRouteData = ()=>{
+  const [showAlert] = useIonAlert();
+  const getRoutes = async (): Promise<any[]> =>{
+    try{
+      return (await database.listDocuments(DB_ID, ROUTES_COLLECTION_ID)).documents
+    }
+    catch(e){
+      showAlert(`Error fetching the routes: ${e}` )
+      return []
+    }
+  }
+  const getRouteById = async (id: string): Promise<any|null> =>{
+    try{
+      return database.getDocument(DB_ID, ROUTES_COLLECTION_ID, id)
+    }
+    catch(e){
+      showAlert(`Error fetching the route with id ${id}: ${e}`)
+      return null;
+    }
+  }
+  const createRoute = async (name: string, starts_at: string, ends_at: string, price: number, longitudes: number[], latitudes: number[]) =>{
+    try{
+      return (await database.createDocument(DB_ID, ROUTES_COLLECTION_ID, 'unique()',
+        {name, starts_at, ends_at, price:Number(price), longitudes, latitudes},
+        [Permission.read(Role.users()), Permission.delete(Role.label('admin')), Permission.update(Role.label('admin'))])
+      )
+    }
+    catch(e){
+      showAlert(`Error creating the route: ${e}`)
+      return null;
+    }
+  }
+  const updateRouteById = async (id: string, route: Route): Promise<any|null> =>{
+    try{
+      return database.updateDocument(DB_ID, ROUTES_COLLECTION_ID, id, route)
+    }
+    catch(e){
+      showAlert(`Error updating the route with id ${id}: ${e}`)
+      return null;
+    }
+  }
+  const deleteRouteById = async (id: string): Promise<any|null> =>{
+    try{
+      await database.deleteDocument(DB_ID, ROUTES_COLLECTION_ID, id)
+    }
+    catch(e){
+      showAlert(`Error updating the route with id ${id}: ${e}`)
+    }
+  }
+  return {
+    getRoutes,
+    getRouteById,
+    createRoute,
+    updateRouteById,
+    deleteRouteById
+  }
+}
+
+
--- src/pages/AddRoute.tsx
+++ src/pages/AddRoute.tsx
@@ -1,12 +1,29 @@
-import { IonContent, IonRange, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonRange, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonButton, IonIcon, IonGrid, IonRow, IonCol, IonDatetime } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
 import { useState } from 'react';
+import { useRouteData} from '../hooks/routeData';
+import { useForm } from 'react-hook-form';
+import { useIonLoading, useIonAlert } from '@ionic/react';
 
 const AddRoute: React.FC = () => {
+  const [present, dismiss] = useIonLoading();
+  const [presentAlert, dismissAlert] = useIonAlert()
   const [pointsInputs, setPointsInputs] = useState([{name:'point1'}]);
+  const {handleSubmit, register, getValues, setValue} = useForm();
   const count = pointsInputs.length
+  const extractDateFromEvent = (ev)=>ev.detail.value?.split('T')[1] ?? '';
   const addPointInput = ()=>{
     setPointsInputs([...pointsInputs, {name:`point${count}`}])
+  }
+  const {createRoute} = useRouteData();
+  const submitNewRoute = async ()=>{
+    const data = getValues()
+    await present()
+    await createRoute(data.name, data.starts_at, data.ends_at, data.price, data.longitudes, data.latitudes);
+    dismiss()
+  }
+  const displayError = (error: any)=>{
+    presentAlert(`Error: ${error}`)
   }
   return (
     <IonPage>
@@ -16,41 +33,65 @@ const AddRoute: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonItem>
-          <IonLabel position="stacked">Nombre de la Ruta</IonLabel>
-          <IonInput placeholder="Ingresa el nombre de la ruta" type="text" minlength={3}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="stacked">Horario</IonLabel>
-          <IonRange aria-label="Range with ticks" pin={true} pinFormatter={(value)=>`${value} H`} ticks={true} dualKnobs={true} snaps={true} min={0} max={23}></IonRange>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="stacked">Precio</IonLabel>
-          <IonInput placeholder="Ingresa el precio" type="number" min={0}></IonInput>
-        </IonItem>
-        <IonItem>Puntos en la ruta: {count}</IonItem>
-        <IonGrid>
-        {
-          pointsInputs.map((element, index) => {
-            const human_index = index + 1;
-            return (
-              <IonRow class='ion-align-items-center ion-justify-content-center'>
-                <IonCol size='2'>
-                  {human_index}
+        <form onSubmit={handleSubmit(submitNewRoute, displayError)}>
+          <IonItem>
+            <IonLabel position="stacked">Nombre de la Ruta</IonLabel>
+            <IonInput id='name' {...register('name', {required:true, maxLength: 126})} placeholder="Ingresa el nombre de la ruta" type="text" minlength={3}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                    <IonLabel>Inicio</IonLabel>
                 </IonCol>
-                <IonCol size='5'>
-                  <IonInput name={`longitude_${index}`} type='number' min={-90} max={90} placeholder={`Longitud ${human_index}`}></IonInput>
-                </IonCol>
-                <IonCol size='5'>
-                  <IonInput name={`latitude_${index}`} type='number' min={-90} max={90} placeholder={`Latitud ${human_index}`}></IonInput>
+                <IonCol>
+                    <IonLabel>Fin</IonLabel>
                 </IonCol>
               </IonRow>
-            );
-          })
-        }
-        </IonGrid>
-        <IonButton onClick={addPointInput} ><IonIcon icon={addOutline} slot='start'></IonIcon>Agregar punto</IonButton>
-        <IonButton expand="block">Guardar Ruta</IonButton>
+              <IonRow>
+                <IonCol>
+                  <IonDatetime id='startsAt' presentation='time' name='starts_at' onIonChange={(ev)=>setValue('starts_at', extractDateFromEvent(ev))}></IonDatetime>
+                </IonCol>
+                <IonCol>
+                  <IonDatetime id='endsAt' presentation='time' name='ends_at' onIonChange={(ev)=>setValue('ends_at', extractDateFromEvent(ev))}></IonDatetime>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Precio</IonLabel>
+            <IonInput id='price' placeholder="Ingresa el precio" type="number" step='any' {... register('price', {required:true, min:0})}></IonInput>
+          </IonItem>
+          <IonItem>Puntos en la ruta: {count}</IonItem>
+          <IonGrid>
+          {
+            pointsInputs.map((element, index) => {
+              const human_index = index + 1;
+              return (
+                <IonRow class='ion-align-items-center ion-justify-content-center'>
+                  <IonCol size='2'>
+                    {human_index}
+                  </IonCol>
+                  <IonCol size='5'>
+                    <IonInput id={`longitude${index}`} {...register(`longitudes${index}`, {required:true})} type='number' step='any' min={-180} max={180} placeholder={`Longitud ${human_index}`}></IonInput>
+                  </IonCol>
+                  <IonCol size='5'>
+                    <IonInput id={`latitude${index}`}  {...register(`latitudes.${index}`, {required:true})} type='number' step='any' min={-90} max={90} placeholder={`Latitud ${human_index}`}></IonInput>
+                  </IonCol>
+                </IonRow>
+              );
+            })
+          }
+            <IonRow class='ion-justify-content-center'>
+              <IonCol size='8'>
+                <IonButton id='addPoint' type='button' onClick={addPointInput} expand='block' color='secondary'>
+                  <IonIcon icon={addOutline} slot='start'></IonIcon>Agregar punto
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          <IonButton id='submit' type='submit' expand="block">Guardar Ruta</IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
