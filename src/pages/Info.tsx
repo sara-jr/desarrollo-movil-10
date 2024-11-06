3743e3c43cc4336415933315597fbbf91042e925
sara-jr <salazar.ramirez.j@hotmail.com>
2024-11-06 10:42:30 -0600

    Modificado el layout de la pagina de detalles a pestañas y agregada la linea de ruta en el mapa

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton, IonTabs, IonTabBar, IonTabButton, IonLabel, IonTab, IonItem, IonText, IonSpinner, IonGrid, IonCol, IonRow } from '@ionic/react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton, IonTabs, IonTabBar, IonTabButton, IonLabel, IonTab, IonItem, IonText, IonSpinner, IonGrid, IonCol, IonRow, IonCard, IonCardHeader, IonCardContent } from '@ionic/react';
+import { MapContainer, TileLayer, Marker, Popup, useMap, Polygon } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import { useRouteData } from '../hooks/routeData';
 import { useIonAlert, useIonLoading } from '@ionic/react';
@@ -12,7 +12,6 @@ const Info: React.FC = () => {
     const [route, setRoute] = useState(null as any)
     const [presentAlert] = useIonAlert();
     const {id} = useParams<{id:string}>();
-  
     const fetchRouteData = async ()=>{
       try{
         const data = await getRouteById(id)
@@ -30,87 +29,61 @@ const Info: React.FC = () => {
         </IonContent>
       ) 
     }
-    const routeName = route.name;
-
-    // Información para cada pestaña
-    const generalInfo = "Información general sobre la ruta. Aquí puedes ver detalles sobre el recorrido y las paradas.";
-    const pricingInfo = "Precios aproximados para viajar en esta ruta. Asegúrate de verificar con el conductor.";
-    const reportInfo = "Reportes sobre la ruta, incidencias, horarios y más.";
-  
-    // Coordenadas de ejemplo (puedes cambiarlas por las de tus rutas de autobuses)
-    const initialPosition: LatLngExpression = [route.latitudes[0], route.longitudes[0]]; // Coordenadas de CDMX
-    let markers = [];
-    for(let i = 0; i < route.latitudes.length; i++){
-      markers.push({lat:route.latitudes[i], lng:route.longitudes[i], title:`Parada ${i}`})
-    }
-  
+    const initialPosition: LatLngExpression = [route.latitudes[0], route.longitudes[0]]; 
+    const markers = route.latitudes.map((value: any, index: number)=>({lat:value, lng:route.longitudes[index], title:`Parada ${index}`}))
     return (
       <IonPage>
         <IonHeader>
           <IonToolbar>
             <IonButtons slot="start">
-              <IonBackButton />
+              <IonBackButton></IonBackButton>
             </IonButtons>
-            <IonTitle>{routeName}</IonTitle>
+            <IonTitle>Ruta: {route.name}</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <MapContainer
-                  center={initialPosition}
-                  zoom={12}
-                  style={{ width: '100%', height: '300px', marginBottom: '20px' }}
-                >
-                  <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="© OpenStreetMap contributors"
-                  />
-                  {markers.map((marker, index) => (
-                    <Marker key={index} position={[marker.lat, marker.lng]}>
-                      <Popup>{marker.title}</Popup>
-                    </Marker>
-                  ))}
-                </MapContainer>
-              </IonCol>
-            </IonRow>
-    
-
-            {/* Pestañas de información */}
-            <IonRow>
-              <IonCol>
-                  <IonTabs>
-                    <IonTabBar slot="top">
-                      <IonTabButton tab="general">
-                        <IonLabel>General</IonLabel>
-                      </IonTabButton>
-                      <IonTabButton tab="pricing">
-                        <IonLabel>Precios</IonLabel>
-                      </IonTabButton>
-                      <IonTabButton tab="reports">
-                        <IonLabel>Reportes</IonLabel>
-                      </IonTabButton>
-                    </IonTabBar>
-                    <IonTab tab="general">
-                      <IonItem>
-                        <IonText>{generalInfo}</IonText>
-                      </IonItem>
-                    </IonTab>
-                    <IonTab tab="pricing">
-                      <IonItem>
-                        <IonText>{route.price}</IonText>
-                      </IonItem>
-                    </IonTab>
-                    <IonTab tab="reports">
-                      <IonItem>
-                        <IonText>{reportInfo}</IonText>
-                      </IonItem>
-                    </IonTab>
-                  </IonTabs>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
+          <IonTabs>
+            <IonTabBar slot='bottom'>
+              <IonTabButton tab="map">
+                <IonLabel>Mapa</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="general">
+                <IonLabel>General</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="pricing">
+                <IonLabel>Precios</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="reports">
+                <IonLabel>Reportes</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+            <IonTab tab='map'>
+              <MapContainer center={initialPosition} zoom={60} style={{ width: '100%', height: '100%', marginBottom: '20px' }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="© OpenStreetMap contributors" />
+                {markers.map((marker: any, index: number) => (
+                  <Marker key={index} position={[marker.lat, marker.lng]}>
+                    <Popup>{marker.title}</Popup>
+                  </Marker>
+                ))}
+                <Polygon positions={markers} pathOptions={{color:'orange'}}></Polygon>
+              </MapContainer>
+            </IonTab>
+            <IonTab tab="general">
+              <IonItem>
+                <IonText>{}</IonText>
+              </IonItem>
+            </IonTab>
+            <IonTab tab="pricing">
+              <IonItem>
+                <IonText>Precio base: {route.price}</IonText>
+              </IonItem>
+            </IonTab>
+            <IonTab tab="reports">
+              <IonItem>
+                <IonText>{}</IonText>
+              </IonItem>
+            </IonTab>
+          </IonTabs>
         </IonContent>
       </IonPage>
     );
